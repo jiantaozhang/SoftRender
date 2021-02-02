bd6df3446a01be807f0ea3eed4358edbe2269773
jiantaozhang <jian_tao_zhang@hotmail.com>
2021-02-02 19:23:22 +0800

    init

---
+++ SoftRender/SoftRender/common.h
@@ -0,0 +1,42 @@
+﻿
+void line(int x1, int y1, int x2, int y2, TGAImage& image, TGAColor color) {
+	// 处理斜率的绝对值大于 1 的直线
+	bool steep = false;
+	if (std::abs(x1 - x2) < std::abs(y1 - y2)) {
+		std::swap(x1, y1);
+		std::swap(x2, y2);
+		steep = true;
+	}
+	// 交换起点终点的坐标
+	if (x1 > x2) {
+		std::swap(x1, x2);
+		std::swap(y1, y2);
+	}
+	int y = y1;
+	int eps = 0;
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int yi = 1;
+
+	// 处理 [-1, 0] 范围内的斜率
+	if (dy < 0) {
+		yi = -1;
+		dy = -dy;
+	}
+
+	for (int x = x1; x <= x2; x++) {
+		if (steep) {
+			image.set(y, x, color);
+		}
+		else {
+			image.set(x, y, color);
+		}
+
+		eps += dy;
+		// 这里用位运算 <<1 代替 *2
+		if ((eps << 1) >= dx) {
+			y = y + yi;
+			eps -= dx;
+		}
+	}
+}
---
+++ SoftRender/SoftRender/main.cpp
@@ -0,0 +1,39 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+
+#include "tgaimage.h"
+#include "model.h"
+#include "common.h"
+
+
+const TGAColor red = TGAColor(255, 0, 0, 255);
+//const TGAColor rr(255, 0, 0, 255);
+
+
+int main(int argc, char** argv)
+{
+	// load
+	Model* m = new Model("obj/african_head.obj");
+
+	// iterate model triangles
+	int nfaces = m->nfaces();
+	for (int i = 0; i < nfaces; i++)
+	{
+
+		std::vector<int> face = m->m
+
+	}
+
+
+
+	TGAImage img(100, 100, TGAImage::RGB);
+
+	line(25, 25, 75, 75, img, red);
+
+	img.flip_vertically();
+	img.write_tga_file("../output/e1.tga");
+
+	return 0;
+}
